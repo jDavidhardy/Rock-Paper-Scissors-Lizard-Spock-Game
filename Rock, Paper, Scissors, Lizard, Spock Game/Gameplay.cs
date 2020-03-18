f659e3b7ade2c60f320b0dddfd29ce0b91c6188b
unknown <jhard5134@gmail.com>
2020-03-17 23:31:27 -0500

    initial start page done. gampelay start needed still

--- Rock, Paper, Scissors, Lizard, Spock Game/Gameplay.cs
+++ Rock, Paper, Scissors, Lizard, Spock Game/Gameplay.cs
@@ -23,15 +23,24 @@ namespace Rock__Paper__Scissors__Lizard__Spock_Game
        
         public void GameplayRules()
         {
-            Console.WriteLine("Let's play the 'Big Bang Theory shows's game of:  Rock-Paper-Scissors-Lizard-Spock");
-            Console.WriteLine("======================================================================================================================");
-            Console.WriteLine("GAME RULES");
-            Console.WriteLine("Rock beats: Scissors, Lizard");
-            Console.WriteLine("Paper beats: Rock, Spock                              GESTURE SELECTION");
-            Console.WriteLine("Scissors beats: Lizard, Paper                          1 = Rock, 2 = Paper, 3 = Scissors, 4 = Lizard, 5 = Spock");
-            Console.WriteLine("Lizard beats: Paper, Spock                              S = Single Player");
-            Console.WriteLine("Spock beats: Rock, Scissors                             M = Multiplayer");
+            Console.WriteLine("                   Let's play the 'Big Bang Theory shows's game of:  Rock-Paper-Scissors-Lizard-Spock");
+            Console.WriteLine("_______________________________________________________________________________________________________________________");
+            Console.WriteLine("     B     B      T              'Scissors cuts paper, paper covers rock,                                              ");
+            Console.WriteLine("      I     A      H              rock crushes lizard, lizard poisons Spock,                                           ");
+            Console.WriteLine("       G     N      E            Spock smashes scissors, scissors decapitate lizard,                                   ");
+            Console.WriteLine("              G      O          lizard eats paper, paper disproves Spock,                                              ");
+            Console.WriteLine("                      R         Spock vaporizes rock.And as it always has, rock crushes scissors.'                     ");    
+            Console.WriteLine("                       Y                  --Dr. Sheldon Cooper                                                         ");
+            Console.WriteLine("=======================================================================================================================");
+            Console.WriteLine(" Game Rules                                                                                                            ");
+            Console.WriteLine("Rock beats: Scissors, Lizard                                                                                           ");
+            Console.WriteLine("Paper beats: Rock, Spock                                                 GESTURE SELECTION                             ");
+            Console.WriteLine("Scissors beats: Lizard, Paper                          1 = Rock, 2 = Paper, 3 = Scissors, 4 = Lizard, 5 = Spock        ");
+            Console.WriteLine("Lizard beats: Paper, Spock                                               S = Single Player                             ");
+            Console.WriteLine("Spock beats: Rock, Scissors                                              M = Multiplayer                               ");
             Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("                                                  HIT ENTER TO BEGIN!                                                  ");
+
             Console.ReadLine();
 
             GameplayRules();
